cfa35a4e51343cc29065ffcaa5db345624cf8c46
Over1ord1022 <149471538+Over1ord1022@users.noreply.github.com>
2025-09-21 14:51:30 +0500

    models.py
    1

--- blogproject/blogapp/models.py
+++ blogproject/blogapp/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.name
+
+class BlogPost(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(upload_to='blog_images/', null=True, blank=True)
+    
+    def __str__(self):
+        return self.title
+
+class UserPreferences(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    theme = models.CharField(max_length=20, default='light')
+    language = models.CharField(max_length=10, default='ru')
+    preferred_categories = models.ManyToManyField(Category, blank=True)
+    
+    def __str__(self):
+        return f"{self.user.username} preferences"
